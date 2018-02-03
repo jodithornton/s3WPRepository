c3c2f90e6c2942739ae5dfc4bb6a3b60faa016b6
Jodi <joday_@hotmail.com>
2018-02-03 23:53:18 +1100

    removed faulty parameter code

--- wp-content/themes/broker-child/functions.php
+++ wp-content/themes/broker-child/functions.php
@@ -23,14 +23,4 @@ function cg_reorder_child_css() {
 add_action( 'wp_enqueue_scripts', 'cg_reorder_child_css', 101 );
 
 
-//Remove query parameters from URLS
-function _remove_script_version( $src ){ 
-	$parts = explode( '?', $src ); 	
-	return $parts[0]; 
-} 
-
-add_filter( 'script_loader_src', '_remove_script_version', 15, 1 ); 
-add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
-
-
 ?>
